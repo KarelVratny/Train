ca8415d43d8e04b581433da0c61b8386ecb80718
KarelVratny <karelvratny@gmail.com>
2024-05-06 09:21:22 +0200

    PersonalWagon

---
+++ Train/PersonalWagon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train;
+public class PersonalWagon {
+    private List<Door> doors;
+    private List<Chair> sits;
+    private int numberOfChairs;
+
+    public List<Door> Doors { get => doors; set => doors = value; }
+    public List<Chair> Sits { get => sits; set => sits = value; }
+    public int NumberOfChairs { get => numberOfChairs; set => numberOfChairs = value; }
+
+    public PersonalWagon(int numberOfChairs) {
+        this.numberOfChairs = numberOfChairs;
+        sits = new List<Chair>(numberOfChairs);
+        doors = new List<Door>();
+    }
+}
