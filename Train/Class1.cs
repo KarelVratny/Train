04eb33b140e9a352758e6c363ae1a9e0b7af90fa
KarelVratny <karelvratny@gmail.com>
2024-05-06 08:43:56 +0200

    Locomotive

---
+++ Train/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train;
+public class Locomotive {
+    private Person driver;
+    private Engine engine;
+
+    public Person Driver { get => driver; set => driver = value; }
+    public Engine Engine { get => engine; set => engine = value; }
+
+    public Locomotive() { }
+    public Locomotive(Person driver, Engine engine) {
+        Driver = driver;
+        Engine = engine;
+    }
+    public override string ToString() {
+        return $"Vlak {Engine}, strojvudce: {Driver}";
+    }
+}
