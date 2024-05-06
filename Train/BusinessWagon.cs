3548e9c54d5fd82f3f4044289a55ac6b5b33626a
KarelVratny <karelvratny@gmail.com>
2024-05-06 09:28:22 +0200

    Steward

---
+++ Train/BusinessWagon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train;
+public class BussinessWagon : PersonalWagon {
+    private Person steward;
+
+    public Person Steward { get => steward; set => steward = value; }
+
+    public BussinessWagon(Person steward, int numberOfchairs) : base(numberOfchairs) {
+        this.steward = steward;
+    }
+    public override string ToString() {
+        return $"Business vagon. {NumberOfChairs} mist k sezeni. Stevard {Steward}";
+    }
+}
