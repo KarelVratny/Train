809f6ad5b3d07cdb276a931a061f3b0e2e44d5b1
KarelVratny <karelvratny@gmail.com>
2024-05-06 09:33:03 +0200

    NighWagon

---
+++ Train/NightWagon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train;
+public class NightWagon : PersonalWagon {
+    private Bed[] beds;
+    private int numberOfBeds;
+
+    public Bed[] Beds { get => beds; set => beds = value; }
+    public int NumberOfBeds { get => numberOfBeds; set => numberOfBeds = value; }
+    public NightWagon(int numberOfChairs, int numberOfBeds) : base(numberOfChairs) {
+        this.numberOfBeds = numberOfBeds;
+        beds = new Bed[numberOfBeds];
+    }
+    public override string ToString() {
+        return $"Nocni vagon. {NumberOfChairs} mist k sezeni. {NumberOfBeds} posteli";
+    }
+}
